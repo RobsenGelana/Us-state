db70068fc6ca9e0d553c5231342e0378faac4c71
RobsenGelana <robsengelana21@gmail.com>
2022-09-24 22:19:01 +0300

    creating a screen and adding the image

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title('U.S. State Game')
+# turtle.setup(width=600, height=600)
+image = 'blank_states_img.gif'
+turtle.addshape(image)
+turtle.shape(image)
+
+
+
+
+screen.exitonclick()
