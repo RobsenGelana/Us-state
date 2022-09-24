9753db1d5b44590adce2f9d546f0aa89df4d4c69
RobsenGelana <robsengelana21@gmail.com>
2022-09-24 22:44:23 +0300

    reading csv files

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import turtle
-import pandas
+import pandas as pd 
 
 screen = turtle.Screen()
 screen.title('U.S. State Game')
-# turtle.setup(width=600, height=600)
 image = 'blank_states_img.gif'
 turtle.addshape(image)
 turtle.shape(image)
 
-user_guess = screen.textinput(title='Guess the state', prompt="What's other state name? ")
+user_guess = screen.textinput(title='Guess the state', prompt="What's other state name? ").title()
+
+data = pd.read_csv('50_states.csv')
+state_data = data['state']
+x_data = data['x']
+y_data = data['y']
