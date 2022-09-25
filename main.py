67a47e8a43c1ff79c222c1c0cd778ffdb568f9ae
RobsenGelana <robsengelana21@gmail.com>
2022-09-25 12:33:49 +0300

    creating a class for the all function

--- main.py
+++ main.py
@@ -14,23 +14,28 @@ FONT = ('arial',8,'bold')
 
 data = pd.read_csv('50_states.csv')
 state_data = data['state']
+class Map:
+    def __init__(self) -> None:
+        self.score = 0
+        self.user_guessed = []
+    def user_input(self):
+        user_guess = screen.textinput(title=f"{self.score}/ {len(state_data)} Guess the state", prompt="What's other state name? ").title()
+        for state in state_data:
+            if user_guess in state:
+                df = data[data['state'] == f"{user_guess}"]
+                x_pos = int(df['x'])
+                y_pos = int(df['y'])
+                turtle.hideturtle()
+                turtle.penup()
+                turtle.setposition(x_pos, y_pos)
+                turtle.write(f'{user_guess}', font=FONT)
+                self.user_guessed.append(user_guess)
+                
 
-def user_input():
-    user_guess = screen.textinput(title=f" Guess the state", prompt="What's other state name? ").title()
-    for state in state_data:
-        if user_guess in state:
-            df = data[data['state'] == f"{user_guess}"]
-            x_pos = int(df['x'])
-            y_pos = int(df['y'])
-            turtle.hideturtle()
-            turtle.penup()
-            turtle.setposition(x_pos, y_pos)
-            turtle.write(f'{user_guess}', font=FONT)
-          
 
-   
+map = Map()
 
 
 game_on = True
 while game_on:
-    user_input()
+    map.user_input()
