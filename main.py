888dd87af9a39fc5ed07b33f75f564a9cba77070
RobsenGelana <robsengelana21@gmail.com>
2022-09-25 13:08:42 +0300

    tracking the score

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ screen.title('U.S. State Game')
 image = 'blank_states_img.gif'
 turtle.addshape(image)
 turtle.shape(image)
-turtle = turtle.Turtle()
+
 
 
 FONT = ('arial',8,'bold')
@@ -16,22 +16,21 @@ data = pd.read_csv('50_states.csv')
 state_data = data['state']
 class Map:
     def __init__(self) -> None:
-        self.score = 0
         self.user_guessed = []
     def user_input(self):
-        user_guess = screen.textinput(title=f"{self.score}/ {len(state_data)} Guess the state", prompt="What's other state name? ").title()
+        user_guess = screen.textinput(title=f"{len(self.user_guessed)}/ {len(state_data)} State Correct", prompt="What's other state name? ").title()
         for state in state_data:
             if user_guess in state:
+                self.user_guessed.append(user_guess)
                 df = data[data['state'] == f"{user_guess}"]
                 x_pos = int(df['x'])
                 y_pos = int(df['y'])
-                turtle.hideturtle()
-                turtle.penup()
-                turtle.setposition(x_pos, y_pos)
-                turtle.write(f'{user_guess}', font=FONT)
-                self.user_guessed.append(user_guess)
+                t = turtle.Turtle()
+                t.hideturtle()
+                t.penup()
+                t.setposition(x_pos, y_pos)
+                t.write(f'{user_guess}', font=FONT)
                 
-
 
 map = Map()
 
